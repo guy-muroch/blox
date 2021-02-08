041130604648c112d2c9824e9ce9bfcc9f8e8479
Vadim Chumak <vadim.inc@gmail.com>
2021-02-08 19:48:49 +0100

    fix console

--- app/backend/services/key-vault/key-vault.service.ts
+++ app/backend/services/key-vault/key-vault.service.ts
@@ -158,7 +158,7 @@ export default class KeyVaultService {
     const keyVaultVersion = await this.versionService.getLatestKeyVaultVersion();
     const envKey = (Connection.db(this.storePrefix).get('env') || 'production');
     const dockerHubImage = `bloxstaking/key-vault${envKey === 'production' ? '' : '-rc'}:${keyVaultVersion}`;
-    console.log('====dockerHubImage====', dockerHubImage);
+    this.logger.info(`Going to run docker based on ${dockerHubImage} keyvault image`);
     const dockerCMD = 'docker start key_vault 2>/dev/null || ' +
       `docker pull  ${dockerHubImage} && docker run -d --restart unless-stopped --cap-add=IPC_LOCK --name=key_vault ` +
       '-v $(pwd)/data:/data ' +
