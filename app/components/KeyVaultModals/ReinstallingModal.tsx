a97b554d38b2b0bd00d13b927854b7af7798b55c
Dmitri Meshin <dmitri@blox.io>
2021-03-11 01:23:51 +0200

    BLOXINF-1670: upgrade or reinstall according to the minor/major/modification version

--- app/components/KeyVaultModals/ReinstallingModal.tsx
+++ app/components/KeyVaultModals/ReinstallingModal.tsx
@@ -1,13 +1,54 @@
 import React, { useEffect } from 'react';
-import { ProcessLoader, ModalTemplate } from 'common/components';
-import useProcessRunner from 'components/ProcessRunner/useProcessRunner';
-import { Title, Description, SmallText, Wrapper } from 'common/components/ModalTemplate/components';
-import Connection from 'backend/common/store-manager/connection';
+import { connect } from 'react-redux';
+import { Log } from '~app/backend/common/logger/logger';
+import * as wizardSelectors from '~app/components/Wizard/selectors';
+import { ProcessLoader, ModalTemplate } from '~app/common/components';
+import useProcessRunner from '~app/components/ProcessRunner/useProcessRunner';
+import {
+  Title,
+  Description,
+  SmallText,
+  Wrapper
+} from '~app/common/components/ModalTemplate/components';
+import * as keyVaultSelectors from '~app/components/KeyVaultManagement/selectors';
+
+enum ProcessName {
+  UPGRADE = 'upgrade',
+  REINSTALL = 'reinstall'
+}
+
+const logger = new Log('ReinstallingModal');
+
+const getProcessName = (keyVaultCurrentVersion: string, keyVaultLatestVersion: string): ProcessName => {
+  if (keyVaultCurrentVersion === keyVaultLatestVersion) {
+    return ProcessName.REINSTALL;
+  }
+  const versionRegexp = /(v)?((?<major>\d+)\.)?((?<minor>\d+)\.)?(?<mod>\d+)$/mgi;
+  const parsedVersions = {
+    current: new RegExp(versionRegexp).exec(keyVaultCurrentVersion)?.groups ?? null,
+    latest: new RegExp(versionRegexp).exec(keyVaultLatestVersion)?.groups ?? null
+  };
+  if (!parsedVersions.current?.major || !parsedVersions.latest?.major) {
+    return ProcessName.REINSTALL;
+  }
+  if (parseInt(parsedVersions.latest.major, 10) > parseInt(parsedVersions.current.major, 10)) {
+    return ProcessName.UPGRADE;
+  }
+  if (!parsedVersions.current?.minor || !parsedVersions.latest?.minor) {
+    return ProcessName.REINSTALL;
+  }
+  if (parseInt(parsedVersions.latest.minor, 10) > parseInt(parsedVersions.current.minor, 10)) {
+    return ProcessName.UPGRADE;
+  }
+  return ProcessName.REINSTALL;
+};
 
 const ReinstallingModal = (props: Props) => {
   const { isLoading, processMessage, isDone, isServerActive, processName,
     startProcess, clearProcessState, loaderPercentage } = useProcessRunner();
-  const { title, description, move1StepForward, move2StepsForward, onClose, image } = props;
+  const {
+    title, description, move1StepForward, move2StepsForward,
+    onClose, image, keyVaultCurrentVersion, keyVaultLatestVersion } = props;
 
   useEffect(() => {
     if (isDone) {
@@ -15,10 +56,13 @@ const ReinstallingModal = (props: Props) => {
       isServerActive ? move1StepForward() : move2StepsForward();
     }
     if (!isDone && !isLoading && !processMessage && !processName) {
-      const name = Connection.db().exists('upgradeRatherReinstall') ? 'upgrade' : 'reinstall';
+      const name = getProcessName(keyVaultCurrentVersion, keyVaultLatestVersion);
+      logger.debug(`Current Version: ${keyVaultCurrentVersion}`);
+      logger.debug(`Latest Version: ${keyVaultLatestVersion}`);
+      logger.debug(`Identified Process Name: ${name}`);
       startProcess(name, 'Checking KeyVault configuration...');
     }
-  }, [isLoading, isDone, processMessage]);
+  }, [isLoading, isDone, processMessage, keyVaultCurrentVersion, keyVaultLatestVersion]);
 
   return (
     <ModalTemplate onClose={onClose} image={image}>
@@ -35,10 +79,19 @@ const ReinstallingModal = (props: Props) => {
 type Props = {
   image: string;
   title: string;
+  keyVaultCurrentVersion: string;
+  keyVaultLatestVersion: string;
   description?: string;
   move1StepForward: () => void;
   move2StepsForward: () => void;
   onClose?: () => void;
 };
 
-export default ReinstallingModal;
+const mapStateToProps = (state: State) => ({
+  keyVaultCurrentVersion: wizardSelectors.getWalletVersion(state),
+  keyVaultLatestVersion: keyVaultSelectors.getLatestVersion(state),
+});
+
+type State = Record<string, any>;
+
+export default connect(mapStateToProps, null)(ReinstallingModal);
