bac56f2bcfde6c4d3235e2d28fb97a8f303abbf8
Dmitri Meshin <dmitri@blox.io>
2021-03-15 12:57:26 +0200

    BLOXINF-1689: fix login for windows, v1.4.1-dev.24

--- app/components/App/service.ts
+++ app/components/App/service.ts
@@ -1,7 +1,10 @@
 import { remote } from 'electron';
 import { notification } from 'antd';
-import { Log } from 'backend/common/logger/logger';
 import queryString from 'query-string';
+import { Log } from '~app/backend/common/logger/logger';
+
+const logger = new Log('deepLink');
+const isWindows = process.platform === 'win32';
 
 export const initApp = () => {
   const placement = 'bottomRight';
@@ -11,10 +14,15 @@ export const initApp = () => {
 export const deepLink = (onSuccess, onFailure) => {
   remote.app.on('open-url', (_event, data) => {
     if (data) {
-      // const questionMarkIndex = data.indexOf('//');
-      // const trimmedCode = data.substring(questionMarkIndex + 2);
-      const [, query] = data.split('//');
-      const params : Record<string, any> = queryString.parse(query);
+      let params: Record<string, any>;
+      if (isWindows) {
+        const questionMarkIndex = data.indexOf('//');
+        const trimmedCode = data.substring(questionMarkIndex + 2);
+        params = queryString.parse(trimmedCode);
+      } else {
+        const [, query] = data.split('//');
+        params = queryString.parse(query);
+      }
       try {
         if (Object.keys(params).length > 0) {
           onSuccess(params);
@@ -29,15 +37,19 @@ export const deepLink = (onSuccess, onFailure) => {
   });
 
   remote.app.on('second-instance', (_event, commandLine) => {
-    const logger = new Log();
     logger.trace(commandLine);
     const cmd = commandLine[2] || commandLine[1];
     if (cmd && cmd.includes('blox-live://')) {
-      // const questionMarkIndex = cmd.indexOf('//');
-      // const trimmedCode = cmd.substring(questionMarkIndex + 2);
-      // const withoutSlash = trimmedCode.slice(0, trimmedCode.length - 1);
-      const [, query] = cmd.split('//');
-      const params : Record<string, any> = queryString.parse(query);
+      let params: Record<string, any>;
+      if (isWindows) {
+        const questionMarkIndex = cmd.indexOf('//');
+        const trimmedCode = cmd.substring(questionMarkIndex + 2);
+        const withoutSlash = trimmedCode.slice(0, trimmedCode.length - 1);
+        params = queryString.parse(withoutSlash);
+      } else {
+        const [, query] = cmd.split('//');
+        params = queryString.parse(query);
+      }
       try {
         if (params) {
           const win = remote.getCurrentWindow();
